e39707f5b4e7d4070f8250517d6923c1d937c72b
omarhaweel <omarhaweel@gmail.com>
2025-06-05 10:07:36 +0200

    CI test

--- src/test/java/com/example/cv_vault/DemoApplicationTests.java
+++ src/test/java/com/example/cv_vault/DemoApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.cv_vault;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev")
 @SpringBootTest
 class DemoApplicationTests {
 
